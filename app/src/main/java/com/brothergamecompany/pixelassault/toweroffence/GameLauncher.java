07097133f1aacb068de6bed123d12a79d4f4fc1a
maxgmer <maxhapeyenka@gmail.com>
2017-07-31 22:31:37 +0300

    Version 1.0. Google auth built. Auth code moved to SignIn class.

--- app/src/main/java/com/brothergamecompany/pixelassault/toweroffence/GameLauncher.java
+++ app/src/main/java/com/brothergamecompany/pixelassault/toweroffence/GameLauncher.java
@@ -24,6 +24,7 @@ import com.brothergamecompany.pixelassault.toweroffence.GameObjects.MapBuilder;
 import com.brothergamecompany.pixelassault.toweroffence.GameScreens.GameScreen;
 import com.brothergamecompany.pixelassault.toweroffence.Other.Assets.Assets;
 import com.brothergamecompany.pixelassault.toweroffence.Other.GameGestureDetector;
+import com.brothergamecompany.pixelassault.toweroffence.Other.Network.SignIn;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -50,21 +51,11 @@ import static android.app.Activity.RESULT_OK;
  * Created by maxgm_umv4xdu on 04.06.2017.
  */
 
-public class GameLauncher extends GLGame implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private static final int RC_SIGN_IN = 20;
-    public GoogleApiClient mGoogleApiClient;
-    GoogleSignInOptions gso;
-    private FirebaseAuth mAuth;
-    private FirebaseUser currentUser;
+public class GameLauncher extends GLGame{
     private boolean firstTimeCreate = true;
-    private static boolean mResolvingError = false;
-
-    private static final String STATE_RESOLVING_ERROR = "resolving_error";
-    private static final String DIALOG_ERROR = "dialog_error";
-    private static final int REQUEST_RESOLVE_ERROR = 1001;
-
     private ScaleGestureDetector mScaleDetector;
     private GestureDetectorCompat mGestureDetector;
+    private SignIn signIn;
 
     @Override
     public Screen getStartScreen() {
@@ -87,32 +78,11 @@ public class GameLauncher extends GLGame implements GoogleApiClient.ConnectionCa
     @Override
     public void onCreate(Bundle b) {
         super.onCreate(b);
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        if (mGoogleApiClient == null) buildGoogleApiClient();
-
+        signIn = new SignIn(this, b);
         GameGestureDetector gestureDetector = new GameGestureDetector(getGLGraphics());
         mGestureDetector = new GestureDetectorCompat(this, gestureDetector.simpleOnGestureListener);
         mScaleDetector = new ScaleGestureDetector(this, gestureDetector.mScaleGestureListener);
-        mResolvingError = b != null
-                && b.getBoolean(STATE_RESOLVING_ERROR, false);
-        mAuth = FirebaseAuth.getInstance();
     }
-
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-    }
-
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
@@ -125,129 +95,28 @@ public class GameLauncher extends GLGame implements GoogleApiClient.ConnectionCa
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
-        signIn();
-    }
-
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Toast.makeText(this, "TempDisconnect", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Toast.makeText(this, "ConnectionFailed", Toast.LENGTH_LONG).show();
-        if (mResolvingError) return;
-        else if (connectionResult.hasResolution()) {
-            try {
-                mResolvingError = true;
-                connectionResult.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
-            } catch (IntentSender.SendIntentException e) {
-                // There was an error with the resolution intent. Try again.
-                mGoogleApiClient.connect();
-            }
-        } else {
-            // Show dialog using GoogleApiAvailability.getErrorDialog()
-            showErrorDialog(connectionResult.getErrorCode());
-            mResolvingError = true;
-        }
-    }
-
-
-    @Override
     protected void onStop() {
         super.onStop();
-        mGoogleApiClient.disconnect();
+        signIn.mGoogleApiClient.disconnect();
     }
 
     @Override
     public void onStart(){
         super.onStart();
-        mGoogleApiClient.connect();
-        currentUser = mAuth.getCurrentUser();
+        signIn.mGoogleApiClient.connect();
     }
 
-    private void showErrorDialog(int errorCode) {
-        // Create a fragment for the error dialog
-        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
-        // Pass the error that should be displayed
-        Bundle args = new Bundle();
-        args.putInt(DIALOG_ERROR, errorCode);
-        dialogFragment.setArguments(args);
-        dialogFragment.show(getSupportFragmentManager(), "errordialog");
-    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_RESOLVE_ERROR) {
-            mResolvingError = false;
-            if (resultCode == RESULT_OK) {
-                // Make sure the app is not already connected or attempting to connect
-                if (!mGoogleApiClient.isConnecting() &&
-                        !mGoogleApiClient.isConnected()) {
-                    mGoogleApiClient.connect();
-                }
-            }
-        }
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-                // Google Sign In failed, update UI appropriately
-                // ...
-                Toast.makeText(GameLauncher.this, "SIGN IN FAIL1", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(GameLauncher.this, "SIGN IN SUCCESS", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(GameLauncher.this, "SIGN IN FAIL2", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        signIn.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
-    }
-    /* A fragment to display an error dialog */
-    public static class ErrorDialogFragment extends DialogFragment {
-        public ErrorDialogFragment() { }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Get the error code and retrieve the appropriate dialog
-            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
-            return GoogleApiAvailability.getInstance().getErrorDialog(
-                    this.getActivity(), errorCode, REQUEST_RESOLVE_ERROR);
-        }
-
-        @Override
-        public void onDismiss(DialogInterface dialog) {
-            GameLauncher.mResolvingError = true;
-        }
+        signIn.onSaveInstanceState(outState);
     }
 }
 
---
+++ app/src/main/java/com/brothergamecompany/pixelassault/toweroffence/Other/Network/SignIn.java
@@ -0,0 +1,183 @@
+package com.brothergamecompany.pixelassault.toweroffence.Other.Network;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
+
+import com.brothergamecompany.pixelassault.R;
+import com.brothergamecompany.pixelassault.toweroffence.GameLauncher;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * Created by maxgm_umv4xdu on 31.07.2017.
+ */
+
+public class SignIn implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final int RC_SIGN_IN = 20;
+    public GoogleApiClient mGoogleApiClient;
+    private GoogleSignInOptions gso;
+    private static boolean mResolvingError = false;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+
+    private static final String STATE_RESOLVING_ERROR = "resolving_error";
+    private static final String DIALOG_ERROR = "dialog_error";
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    private GameLauncher context;
+
+    public SignIn(GameLauncher context, Bundle b) {
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        this.context = context;
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(context.getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        if (mGoogleApiClient == null) buildGoogleApiClient(context);
+        mResolvingError = b != null
+                && b.getBoolean(STATE_RESOLVING_ERROR, false);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    protected synchronized void buildGoogleApiClient(GameLauncher context) {
+        mGoogleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(context, "Connected", Toast.LENGTH_LONG).show();
+        signIn();
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        context.startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(context, "TempDisconnect", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(context, "ConnectionFailed", Toast.LENGTH_LONG).show();
+        if (mResolvingError) return;
+        else if (connectionResult.hasResolution()) {
+            try {
+                mResolvingError = true;
+                connectionResult.startResolutionForResult(context, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            // Show dialog using GoogleApiAvailability.getErrorDialog()
+            showErrorDialog(connectionResult.getErrorCode());
+            mResolvingError = true;
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+                Toast.makeText(context, "SIGN IN FAIL1", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            currentUser = mAuth.getCurrentUser();
+                            Toast.makeText(context, "SIGN IN SUCCESS", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(context, "SIGN IN FAIL2", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(context.getSupportFragmentManager(), "errordialog");
+    }
+
+
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
+    }
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(), errorCode, REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            SignIn.mResolvingError = true;
+        }
+    }
+}
